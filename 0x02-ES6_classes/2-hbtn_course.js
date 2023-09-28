dbef142876a8e8b628711186c8b75848740186aa
OChibu <beze4luv@yahoo.com>
2023-09-28 07:35:40 -0700

    Task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,15 +2,15 @@ import HolbertonCourse from './HolbertonCourse.js';
 
 const course = new HolbertonCourse('JavaScript Basics', 12, ['Alice', 'Bob', 'Charlie']);
 
-console.log(course.name);
-console.log(course.length);
-console.log(course.students);
+console.log(course.name); JavaScript Basics;
+console.log(course.length), 12;
+console.log(course.students); ['Alice', 'Bob', 'Charlie']);
 
 course.name = 'Advanced JavaScript';
-console.log(course.name);
+console.log(course.name);  Advanced JavaScript;
 
 course.length = 15;
-console.log(course.length);
+console.log(course.length),  15;
 
 course.students.push('David');
-console.log(course.students);
+console.log(course.students); ['Alice', 'Bob', 'Charlie', 'David']);
