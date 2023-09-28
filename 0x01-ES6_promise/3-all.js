063db4a01e9af11850958ffadb7379eb218a40d2
OChibu <beze4luv@yahoo.com>
2023-09-28 06:37:04 -0700

    es6 promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      const value = `${result[0].body} ${result[1].firstName} ${result[1].lastName}`;
+      console.log(value);
+      return result;
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,23 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+  return Promise.allSettled(promises).then((value) => {
+    const result = [];
+    value.forEach((res) => {
+      result.push({
+        status: res.status,
+        value: res.value ? res.value : `${res.reason.name}: ${res.reason.message}`,
+      });
+    });
+    return result;
+  });
+}
