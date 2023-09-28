0e575130276207ca46eb3e1d9b37da6fd4adda37
OChibu <beze4luv@yahoo.com>
2023-09-28 16:59:29 -0700

    Es6 Basic

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  let idx = 0;
+  for (const value of array) {
+    newArray[idx] = appendString + value;
+    idx += 1;
+  }
+
+  return newArray;
+}
